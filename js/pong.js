4309ad62f2c60745cdd689b60dd432d442df29f4
Ryan Seys <rsmseys@gmail.com>
2013-04-18 00:36:51 -0400

    Make more dynamic less magic numbers

--- js/pong.js
+++ js/pong.js
@@ -2,30 +2,32 @@ var p1score = 0; //player 1 score
 var p2score = 0; //player 2 score
 var dx = (Math.random() + 1.7) * (Math.random() > 0.5 ? -1 : 1); // direction of ball's next move
 var dy = (Math.random() + 1.5) * (Math.random() > 0.5 ? -1 : 1); // direction of ball's next move
-var offsetx = 0; //offset center x
-var offsety = 0; //offset center y
 var layer1 = new Kinetic.Layer();
 var layer2 = new Kinetic.Layer();
+var w = 600;
+var h = 400;
+var half_w = w/2;
+var half_h = h/2;
 
 var stage = new Kinetic.Stage({
   container: 'container',
-  width: 600,
-  height: 400
+  width: w,
+  height: h
 });
 
 var background1 = new Kinetic.Rect({
-  x:0,
-  y:0,
-  width: 300,
-  height: 400,
+  x: 0,
+  y: 0,
+  width: half_w,
+  height: h,
   fill: 'white'
 });
 
 var background2 = new Kinetic.Rect({
-  x:300,
-  y:0,
-  width: 300,
-  height: 400,
+  x: half_w,
+  y: 0,
+  width: half_w,
+  height: h,
   fill: 'white'
 });
 
@@ -54,31 +56,43 @@ var score2 = new Kinetic.Label({
 });
 
 var centerLine = new Kinetic.Line({
-  points: [300, 0, 300, 400],
+  points: [half_w, 0, half_w, h],
   stroke: 'black'
 });
 
 var player1 = new Kinetic.Rect({
   x: 10,
-  y: 190,
+  y: half_h,
   width: 10,
   height: 50,
-  fill: 'black'
+  fill: 'black',
+  offset : {
+    x: 5,
+    y: 25
+  }
 });
 
 var player2 = new Kinetic.Rect({
   x: 580,
-  y: 190,
+  y: half_h,
   width: 10,
   height: 50,
-  fill: 'black'
+  fill: 'black',
+  offset : {
+    x: 5,
+    y: 25
+  }
 });
 
 var ball = new Kinetic.Circle({
-  x: 300,
-  y: 200,
+  x: half_w,
+  y: half_h,
   radius: 10,
-  fill: 'black'
+  fill: 'black',
+  offset : {
+    x : 5,
+    y : 5
+  }
 });
 
 background1.on('mousemove', function() {
@@ -92,10 +106,8 @@ background2.on('mousemove', function() {
 });
 
 function resetBall() {
-  dx = 0;
-  dy = 0;
-  ball.setX(300);
-  ball.setY(200);
+  ball.setX(half_w);
+  ball.setY(half_h);
   dx = (Math.random() + 1.7) * (Math.random() > 0.5 ? -1 : 1); // direction of ball's next move
   dy = (Math.random() + 1.5) * (Math.random() > 0.5 ? -1 : 1); // direction of ball's next move
 }
@@ -105,16 +117,6 @@ function tick() {
   var x2 = ball.getX() + dx;
   var y2 = ball.getY() + dy;
 
-  //offset for ball edge collision
-  //TODO: make -5 / +5 equal to radius / 2
-  if(dx > 0) offsetx = 5;
-  else offsetx = -5;
-  if(dy > 0) offsety = 5;
-  else offsety = -5;
-
-  x2 += offsetx;
-  y2 += offsety
-
   //paddles and back walls
   if(x2 < 0) {
     //player 2 gets a point!
@@ -123,22 +125,22 @@ function tick() {
     layer1.draw();
     resetBall();
   }
-  else if(x2 > 600) {
+  else if(x2 > w) {
     //player 1 gets a point!
     p1score++;
     score1.getText().setText(p1score.toString());
     layer1.draw();
     resetBall();
   }
-  else if(x2 < 20 && x2 > 15) {
+  else if(x2 < 25 && x2 > 20) {
     var p1y = player1.getY();
-    if(y2 < p1y + 60 && y2 > p1y) {
+    if(y2 > p1y - 25 && y2 < p1y + 25) {
       dx = dx * -1.1;
     }
   }
-  else if(x2 > 580 && x2 < 585) {
+  else if(x2 > (w - 25) && x2 < (w - 20)) {
     var p2y = player2.getY();
-    if(y2 < p2y + 60 && y2 > p2y) {
+    if(y2 > p2y - 25 && y2 < p2y + 25) {
       dx = dx * -1.1;
     }
   }
@@ -147,7 +149,7 @@ function tick() {
   if(y2 < 0) {
     dy = -dy;
   }
-  else if(y2 > 400) {
+  else if(y2 > h) {
     dy = -dy;
   }
 
