31ff83b948fea528d9126086e7acdaa8f03bdee0
Ryan Seys <rsmseys@gmail.com>
2013-04-17 22:15:43 -0400

    Reduce tick function to be more efficient

--- js/pong.js
+++ js/pong.js
@@ -1,15 +1,17 @@
+var p1score = 0; //player 1 score
+var p2score = 0; //player 2 score
+var dx = (Math.random() + 0.7) * (Math.random() > 0.5 ? -1 : 1); // direction of ball's next move
+var dy = (Math.random() + 0.5) * (Math.random() > 0.5 ? -1 : 1); // direction of ball's next move
+var offsetx = 0; //offset center x
+var offsety = 0; //offset center y
+var layer1 = new Kinetic.Layer();
+var layer2 = new Kinetic.Layer();
+
 var stage = new Kinetic.Stage({
   container: 'container',
   width: 600,
   height: 400
 });
-
-var p1score = 0; //player 1 score
-var p2score = 0; //player 2 score
-var dx = (Math.random() + 0.7) * (Math.random() > 0.5 ? -1 : 1); // direction of ball's next move
-var dy = (Math.random() + 0.5) * (Math.random() > 0.5 ? -1 : 1); // direction of ball's next move
-var layer1 = new Kinetic.Layer();
-var layer2 = new Kinetic.Layer();
 
 var background1 = new Kinetic.Rect({
   x:0,
@@ -105,12 +107,9 @@ function resetBall() {
 }
 
 function tick() {
-  var x = ball.getX();
-  var y = ball.getY();
-  var x2 = x + dx;
-  var y2 = y + dy;
-  var offsetx = 0;
-  var offsety = 0;
+  //calc new x and y (x2 and y2)
+  var x2 = ball.getX() + dx;
+  var y2 = ball.getY() + dy;
 
   //offset for ball edge collision
   //TODO: make -5 / +5 equal to radius / 2
@@ -119,28 +118,31 @@ function tick() {
   if(dy > 0) offsety = 5;
   else offsety = -5;
 
+  x2 += offsetx;
+  y2 += offsety
+
   //paddles and back walls
-  if(x2 + offsetx < 0) {
+  if(x2 < 0) {
     //player 2 gets a point!
     p2score++;
     score2.getText().setText(p2score.toString());
     layer1.draw();
     resetBall();
   }
-  else if(x2 + offsetx > 600) {
+  else if(x2 > 600) {
     //player 1 gets a point!
     p1score++;
     score1.getText().setText(p1score.toString());
     layer1.draw();
     resetBall();
   }
-  else if(x2 + offsetx < 20 && x2 + offsetx > 15) {
+  else if(x2 < 20 && x2 > 15) {
     var p1y = player1.getY();
     if(y2 < p1y + 60 && y2 > p1y) {
       dx = dx * -1.1;
     }
   }
-  else if(x2 + offsetx > 580 && x2 + offsetx < 585) {
+  else if(x2 > 580 && x2 < 585) {
     var p2y = player2.getY();
     if(y2 < p2y + 60 && y2 > p2y) {
       dx = dx * -1.1;
@@ -148,10 +150,10 @@ function tick() {
   }
 
   // top and bottom of game walls
-  if(y2 + offsety < 0) {
+  if(y2 < 0) {
     dy = -dy;
   }
-  else if(y2 + offsety > 400) {
+  else if(y2 > 400) {
     dy = -dy;
   }
 
